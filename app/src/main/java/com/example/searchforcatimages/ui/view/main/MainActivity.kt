74cad930aabbe8f13edef2822ada0c237f630b76
caiocesarcosta <kaiocesarcosta@gmail.com>
2024-02-21 12:26:37 -0300

    fixedbug (crash screen)

--- app/src/main/java/com/example/searchforcatimages/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/example/searchforcatimages/ui/view/main/MainActivity.kt
@@ -32,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mainViewModel.loadImgsFromApi()
+
         setupUI()
         observeResults()
     }
